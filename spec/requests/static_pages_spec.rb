8c435744323a303269c563343f44f390c2bfe8b8
akishige <so1itaryrove@gmail.com>
2014-08-11 15:37:46 +0900

    Add ContactPage

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,17 @@ describe "StaticPages" do
 			expect(page).to have_title("About")
 		end
 	end
+
+	describe "Contact" do
+
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Contact")
+		end
+	end
 end
